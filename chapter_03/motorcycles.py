1dd2c6ea74ad8d12ea283c3a3a57522f93d958ad
hamelj1 <48658747+hamelj1@users.noreply.github.com>
2023-04-06 15:20:25 -0400

    Update motorcycles.py

--- chapter_03/motorcycles.py
+++ chapter_03/motorcycles.py
@@ -4,4 +4,4 @@ print(motorcycles)
 too_expensive = 'ducati'
 motorcycles.remove(too_expensive)
 print(motorcycles)
-print(f"\nA {too_expensive.title()} is too expensive for me.")
+print(f"\nA {too_expensive.title()} is too expensive also for me.")
