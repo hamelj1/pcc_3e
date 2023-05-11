75fc22ed1e6015bf0258c063de5b9325eff48cf4
hamelj1 <48658747+hamelj1@users.noreply.github.com>
2023-05-11 08:58:39 -0400

    5-11-2023

--- chapter_16/mapping_global_datasets/partial_programs/eq_explore_data_0_first_version.py
+++ chapter_16/mapping_global_datasets/partial_programs/eq_explore_data_0_first_version.py
@@ -3,11 +3,11 @@ import json
 
 
 # Read data as a string and convert to a Python object.
-path = Path('eq_data/eq_data_1_day_m1.geojson')
-contents = path.read_text()
+path = Path('C:/Users/jhamel/Documents/GitHub/pcc_3e/chapter_16/mapping_global_datasets/partial_programs/eq_data/eq_data_1_day_m1.geojson')
+contents = path.read_text(encoding="utf-8")
 all_eq_data = json.loads(contents)
 
 # Create a more readable version of the data file.
-path = Path('eq_data/readable_eq_data.geojson')
+path = Path('C:/Users/jhamel/Documents/GitHub/pcc_3e/chapter_16/mapping_global_datasets/eq_data/readable_eq_data.geojson')
 readable_contents = json.dumps(all_eq_data, indent=4)
 path.write_text(readable_contents)
--- chapter_16/mapping_global_datasets/partial_programs/eq_explore_data_3_extract_location_data.py
+++ chapter_16/mapping_global_datasets/partial_programs/eq_explore_data_3_extract_location_data.py
@@ -3,8 +3,8 @@ import json
 
 
 # Read data as a string and convert to a Python object.
-path = Path('eq_data/eq_data_1_day_m1.geojson')
-contents = path.read_text()
+path = Path('C:/Users/jhamel/Documents/GitHub/pcc_3e/chapter_16/mapping_global_datasets/partial_programs/eq_data/eq_data_1_day_m1.geojson')
+contents = path.read_text(encoding='utf-8')
 all_eq_data = json.loads(contents)
 
 # Examine all earthquakes in the dataset.
